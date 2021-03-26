b5420d823fa09cbd57db92f4eb5a792cf5a4e128
Ayrton Herrera <Arherzu@gmail.com>
2021-03-25 20:23:18 -0500

    creacion de producto controller

---
+++ appcompras/Controllers/ProductoController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using appcompras.Models;
+
+namespace appcompras.Controllers
+{
+    public class ProductoController : Controller
+    {
+        private readonly ILogger<ProductoController> _logger;
+
+        public ProductoController(ILogger<ProductoController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
